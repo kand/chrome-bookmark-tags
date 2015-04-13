934c3a03f2038eff4a029cb7fc7c189f8d3258ce
Andrew Kos <akos123@gmail.com>
2015-04-12 23:51:57 -0500

    Simple search added, titles now actual links

--- chrome-bookmark-tags.js
+++ chrome-bookmark-tags.js
@@ -1,4 +1,4 @@
-(function (bookmarks) {
+(function (bookmarks, storage) {
 
   var dfs_tree_writer = function (dom_node, root) {
     var i, child;
@@ -11,13 +11,23 @@
 
       // add title to li for this item
       var title_ele = document.createElement('span');
-      title_ele.textContent = child.id + ' - ' + child.title;
+      if (child.url) {
+        // this is an acutal bookmark, use a link
+        var title_a_ele = document.createElement('a');
+        title_a_ele.href = child.url;
+        title_a_ele.textContent = child.title;
+        title_a_ele.target = '_blank';
+        title_ele.appendChild(title_a_ele);
+      } else {
+        // this is a folder, use just the title
+        title_ele.textContent = child.title;
+      }
       li_ele.appendChild(title_ele);
 
       // add input field for bookmarks
       var input_ele = document.createElement('input');
       input_ele.type = 'text';
-      chrome.storage.sync.get(child.id, (function (entry_id, data) {
+      storage.sync.get(child.id, (function (entry_id, data) {
         if (data[entry_id]) {
           this.value = data[entry_id].join(',');
         }
@@ -36,15 +46,15 @@
             // ensure extra spaces around commas are reduced
             .replace(/\s?,\s?/g, ',')
             // turn this into an array
-            .split(' ');
+            .split(',');
 
             // add bookmark tags to storage
             var bookmark_info = {};
             bookmark_info[entry_id] = tags;
-            chrome.storage.sync.set(bookmark_info);
+            storage.sync.set(bookmark_info);
         } else {
           // ensure the storage key is removed
-          chrome.storage.sync.remove(entry_id);
+          storage.sync.remove(entry_id);
         }
       }).bind(input_ele, child.id));
       li_ele.appendChild(save_btn);
@@ -60,7 +70,7 @@
   };
 
   // grab the tree and start writing it out
-  var bookmark_tree = chrome.bookmarks.getTree(function (tree) {
+  var bookmark_tree = bookmarks.getTree(function (tree) {
 
     // find container to store list in
     var bookmark_list = document.getElementById('bookmarkList');
@@ -81,4 +91,85 @@
     bookmark_list.appendChild(ul_ele);
   });
 
-})(chrome.bookmarks);
+  // create a searchable data object for searching tags
+  var tags = {};
+  var tags_keys = [];
+  storage.sync.get(null, function (data) {
+    // loop through all bookmark ids
+    var id;
+    for (id in data) {
+      var bookmark_tags = data[id];
+      var i;
+      for (i = 0; i < bookmark_tags.length; i++) {
+        var tag = bookmark_tags[i];
+        if (tag in tags) {
+          // this tag already exists, just add id
+          tags[tag].push(id);
+        } else {
+          // doesn't exist yet, initialize it
+          tags[tag] = [id];
+        }
+      }
+    }
+
+    // keep a list of all the keys
+    tags_keys = Object.keys(tags);
+  });
+
+  // set up tag search input and search results
+  var search_input = document.getElementById('searchInput');
+  var search_results = document.getElementById('searchResults');
+  search_input.addEventListener('keyup', function () {
+    var query = search_input.value;
+
+    var matching_ids = [];
+    if (query) {
+      // loop through tags and add matching ids
+      var i;
+      for (i = 0; i < tags_keys.length; i++) {
+        // does this tag start with the given query?
+        var tag = tags_keys[i];
+        if (tag.startsWith(query)) {
+          // ensure we're only adding unique ids
+          var j;
+          for (j = 0; j < tags[tag].length; j++) {
+            var id = tags[tag][j];
+            var id_in_matches = false;
+            var k;
+            for (k = 0; k < matching_ids.length; k++) {
+              if (id === matching_ids[k]) {
+                // found a match, we can stop searching
+                id_in_matches = true;
+                break;
+              }
+            }
+
+            if (!id_in_matches) {
+              // didn't find the id in the list yet, add it
+              matching_ids.push(id);
+            }
+          }
+        }
+      }
+    }
+
+    // build search results
+    if (matching_ids.length > 0) {
+      // have at least one match, clear out and rebuild results
+      bookmarks.get(matching_ids, function (matches) {
+        var results_ele = document.createElement('ul');
+        var i;
+        for (i = 0; i < matches.length; i++) {
+          var bookmark_ele = document.createElement('li');
+          bookmark_ele.innerHTML = matches[i].title
+          results_ele.appendChild(bookmark_ele);
+        }
+        search_results.innerHTML = results_ele.outerHTML;
+      });
+    } else {
+      // no results, let user know
+      search_results.innerHTML = 'No search results.';
+    }
+  });
+
+})(chrome.bookmarks, chrome.storage);
